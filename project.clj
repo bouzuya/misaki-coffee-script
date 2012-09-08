33cda63a18d190d091ef3af0a966c8c79cc8ff36
bouzuya <bouzuya@gmail.com>
2012-09-08 17:40:06 +0900

    (0.1.1) fixed typo.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject misaki-coffee-script "0.1.0"
+(defproject misaki-coffee-script "0.1.1"
   :description "misaki-coffee-script is one of misaki's compiler plugin using clj-coffee-script."
-  :url "https://github.com/bouzuya/misaki-coffee"
+  :url "https://github.com/bouzuya/misaki-coffee-script"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
